f039020caf53851b40e6fe429ae3420868164d56
falah_hamad <allahisgreatallahisgreat1@gmail.com>
2026-09-22 00:16:12 +0300

    Add Capacitor Android application configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,18 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.falahhamad.deftaraldeyon',
+  appName: 'دفتر الديون المحاسبي',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https',
+  },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
+  },
+};
+
+export default config;
